b67eff34070cc3f83690489b6706584bb540ae4a
tngusmiso <k1miso012@gmail.com>
2018-10-08 03:55:34 +0900

    ObjArray2,3

---
+++ src/javaPractice/Array/ObjArray3.java
@@ -0,0 +1,23 @@
+package javaPractice.Array;
+
+import java.util.Date;
+import javaPractice.Person.Person;
+
+public class ObjArray3 {
+    public static void main(String[] args) {
+        Object[] a = new Object[8];
+        
+        a[0] = "hello";
+		a[1] = new String("world");
+		a[2] = new Date();
+		a[3] = new Person("홍길동", 13);
+		a[4] = new int[] { 1, 2, 3 };
+		a[5] = new String[] { "one", "two", "three" }; 
+		a[6] = new Integer(3);		//기본자료형 -> 객체 
+		a[7] = new Double(3.14);	//기본자료형 -> 객체 
+        
+		for (Object o : a) {
+            System.out.println(o);
+        }
+    }
+}
