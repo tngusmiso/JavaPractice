61a1e39f7b79eac0ddc4d5abd2617a292dccd184
tngusmiso <k1miso012@gmail.com>
2018-10-28 17:04:15 +0900

    Collection3

---
+++ src/javaPractice/Collection/Collection3.java
@@ -0,0 +1,30 @@
+package javaPractice.Collection;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class Collection3 {
+	static void addData(Collection<String> c) {
+		c.add("one");
+		c.add("two");
+		c.add("three");
+	}
+	
+	static void printCollection(Collection<String> c) {
+		Object[] a = c.toArray();	// 배열로 바꾸기 
+		String s = Arrays.toString(a);	//문자열로 바꾸기 
+		System.out.println(s);
+	}
+	
+	public static void main(String[] args) {
+		Collection<String> c1= new LinkedList<String>();
+		Collection<String> c2 = new ArrayList<String>();
+		
+		addData(c1);
+		addData(c2);
+		printCollection(c1);
+		printCollection(c2);
+	}
+}
