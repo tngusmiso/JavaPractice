6ca4a19959d37aa62397f589a1e55ed4e07ce293
tngusmiso <k1miso012@gmail.com>
2018-10-28 15:52:16 +0900

    Ploymorphism3

---
+++ src/javaPractice/Polymorphism/Polymorphism3.java
@@ -0,0 +1,42 @@
+package javaPractice.Polymorphism;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class Polymorphism3 {
+
+	static void addIntegersToList(List<Integer> list, int count) {
+		for(int i = 0; i < count; ++i) {
+			list.add(i);
+		}
+	}
+	
+	static void removeOddNumbers(List<Integer>list) {
+		Iterator<Integer> iterator = list.iterator();
+		while(iterator.hasNext()) {
+			int i = iterator.next();
+			if(i % 2 == 1)  //홀수
+				iterator.remove();
+		}
+	}
+	
+	static void printList(List<Integer> list) {
+		for(int i:list) {
+			System.out.printf("%d", i);
+		}
+		System.out.println();
+	}
+	
+	static void doSomething(List<Integer> list) {
+		addIntegersToList(list,20);
+		removeOddNumbers(list);
+		printList(list);
+	}
+	
+	public static void main(String[] args) {
+		doSomething(new ArrayList<Integer>());
+		doSomething(new LinkedList<Integer>());
+	}
+}
