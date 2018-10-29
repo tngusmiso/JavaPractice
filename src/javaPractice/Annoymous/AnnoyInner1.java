590a700a99a2372848505066cd551ece1e195378
tngusmiso <k1miso012@gmail.com>
2018-10-29 10:31:20 +0900

    AnnoyInner1

---
+++ src/javaPractice/Annoymous/AnnoyInner1.java
@@ -0,0 +1,43 @@
+package javaPractice.Annoymous;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class AnnoyInner1 extends JFrame{
+
+	String message = "클릭";
+	
+	public AnnoyInner1() {
+		setBounds(100,100,450,300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JButton button = new JButton("버튼");
+		button.setBounds(100,100,100,30);
+		getContentPane().add(button);
+		
+		ActionListener listener = new MyActionListener(this);	// 리스너 객체 생성 
+		button.addActionListener(listener);	// 버튼에 리스너 담기 		
+	}
+	
+	public static void main(String[] args) {
+		AnnoyInner1 window = new AnnoyInner1();
+		window.setVisible(true);
+	}
+	
+}
+
+class MyActionListener implements ActionListener{	// 리스너 인터페이스를 구현
+	
+	AnnoyInner1 window;
+	public MyActionListener(AnnoyInner1 window) {
+		this.window = window;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(window, window.message);	// 버튼이 클릭되면 메시지 박스 띄우기 
+	}
+}
