9a91190e6d94462764cdff024050d32333841758
tngusmiso <k1miso012@gmail.com>
2018-10-29 10:45:57 +0900

    AnnoyInner5

---
+++ src/javaPractice/Annoymous/AnnoyInner5.java
@@ -0,0 +1,30 @@
+package javaPractice.Annoymous;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class AnnoyInner5 extends JFrame{
+
+	String message = "클릭";
+	
+	public AnnoyInner5() {
+		setBounds(100,100,450,300);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+		
+		JButton button = new JButton("버튼");
+		button.setBounds(100,100,100,30);
+		getContentPane().add(button);
+		
+		// 람다 익스프레션 문법을 활용 
+		// 메소드 파라미터와 메소드 본문만 남기고, 나머지 부분은 생략 
+		button.addActionListener(e -> {JOptionPane.showMessageDialog(AnnoyInner5.this, message);} );	
+	}
+	
+	public static void main(String[] args) {
+		AnnoyInner1 window = new AnnoyInner1();
+		window.setVisible(true);
+	}
+
+}
