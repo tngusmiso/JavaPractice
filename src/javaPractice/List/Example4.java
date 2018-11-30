4c002e5bdbfa0d56c1436b7986a043be8ebad6cd
tngusmiso <k1miso012@gmail.com>
2018-11-30 23:51:24 +0900

    List - Example4

---
+++ src/javaPractice/List/Example4.java
@@ -0,0 +1,17 @@
+package javaPractice.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example4 {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("zero");
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		System.out.println(list.indexOf("two"));
+		System.out.println(list.indexOf("three"));
+		System.out.println(list.indexOf("four"));
+	}
+}
