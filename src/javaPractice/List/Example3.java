52fa5deede59799bfbc0a4bd9abffd1c71049e41
tngusmiso <k1miso012@gmail.com>
2018-11-30 23:50:09 +0900

    List - Example3

---
+++ src/javaPractice/List/Example3.java
@@ -0,0 +1,18 @@
+package javaPractice.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example3 {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("one");
+		list.add("three");
+		list.add(0, "zero");
+		list.add(2, "two");
+		for (String s : list)
+			System.out.printf("%s ", s);
+		for (int i = 0; i < list.size(); ++i)
+			System.out.printf("%s ", list.get(i));
+	}
+}
