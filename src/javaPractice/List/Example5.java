876778d9765b8441614ccdb9ee1b57c9e3edd5cc
tngusmiso <k1miso012@gmail.com>
2018-12-06 23:57:22 +0900

    List - Example5

---
+++ src/javaPractice/List/Example5.java
@@ -0,0 +1,17 @@
+package javaPractice.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example5 {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("zero");
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		list.remove("two");
+		for (String s : list)
+			System.out.printf("%s ", s);
+	}
+}
