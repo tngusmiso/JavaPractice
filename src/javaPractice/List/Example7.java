f3e711dd8ce7203e01faa38e7a87c5dfec55d896
tngusmiso <k1miso012@gmail.com>
2018-12-07 00:00:25 +0900

    List - Example7

---
+++ src/javaPractice/List/Example7.java
@@ -0,0 +1,18 @@
+package javaPractice.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example7 {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("zero");
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		list.add("four");
+		List<String> list2 = list.subList(1, 4);
+		for (String s : list2)
+			System.out.printf("%s ", s);
+	}
+}
